f4497231bb12c9043cc418213b021636daf12ca7
adalaber <sarajuarezzarco@gmail.com>
2020-03-10 10:41:38 +0100

    Add final commit

--- main.js
+++ main.js
@@ -10,31 +10,40 @@ console.log(randomNumber);
 //  Recoger￼ valores
 
 const button = document.querySelector('.js-button');
-console.log(button);
 
 const inputNumber = document.querySelector('.js-number');
 
 const clue = document.querySelector('.js-clue');
 
+const intents = document.querySelector('.js-intents');
+
 // FUNCIONES
 
 function getInputNumber() {
   const inputContent = inputNumber.value;
-  console.log(inputContent);
   compareNumber();
+  calculateNumberofClicks();
 }
 
 function compareNumber() {
-  debugger;
   if (randomNumber === parseInt(inputNumber.value)) {
     clue.value = 'Has ganado campeona';
   } else if (randomNumber < parseInt(inputNumber.value) && parseInt(inputNumber.value) < 100) {
     clue.value = 'Demasido alto';
-  } else if (randomNumber > parseInt(inputNumber.value)) {
+  } else if (randomNumber > parseInt(inputNumber.value) && parseInt(inputNumber.value) > 1) {
     clue.value = 'Demasido bajo';
-  } else if (parseInt(inputNumber.value) > 100 && parseInt(inputNumber.value) < 1) {
+  } else if (parseInt(inputNumber.value) > 100 || parseInt(inputNumber.value) < 1) {
     clue.value = 'El número debe estar entre 0 y 100';
   }
 }
+
+let sum = 0;
+// sum = intents.value;
+
+function calculateNumberofClicks() {
+  sum = sum + 1;
+  //   sum = intents.value;
+}
+console.log(calculateNumberofClicks());
 
 button.addEventListener('click', getInputNumber);
