1c0f22ecdf8cb7fa22b201dcb6630ae99208e3b8
adalaber <sarajuarezzarco@gmail.com>
2020-03-10 09:56:21 +0100

    Add conditional wip

--- main.js
+++ main.js
@@ -12,13 +12,29 @@ console.log(randomNumber);
 const button = document.querySelector('.js-button');
 console.log(button);
 
+const inputNumber = document.querySelector('.js-number');
+
+const clue = document.querySelector('.js-clue');
+
 // FUNCIONES
 
 function getInputNumber() {
-  const inputContent = document.querySelector('.js-number');
-  console.log(inputContent.value);
+  const inputContent = inputNumber.value;
+  console.log(inputContent);
+  compareNumber();
 }
 
-function compareNumber() {}
+function compareNumber() {
+  debugger;
+  if (randomNumber === parseInt(inputNumber.value)) {
+    clue.value = 'Has ganado campeona';
+  } else if (randomNumber < parseInt(inputNumber.value) && parseInt(inputNumber.value) < 100) {
+    clue.value = 'Demasido alto';
+  } else if (randomNumber > parseInt(inputNumber.value)) {
+    clue.value = 'Demasido bajo';
+  } else if (parseInt(inputNumber.value) > 100 && parseInt(inputNumber.value) < 1) {
+    clue.value = 'El número debe estar entre 0 y 100';
+  }
+}
 
 button.addEventListener('click', getInputNumber);
