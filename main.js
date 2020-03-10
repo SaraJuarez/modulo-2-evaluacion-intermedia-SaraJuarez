5b6f6f6f5ae2c0b2917e334a3bdbde0da5941ce8
adalaber <sarajuarezzarco@gmail.com>
2020-03-10 09:10:15 +0100

    Add first javascript functions

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 'user strict';
 
+// Generar número aleatorio
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
-
 const randomNumber = getRandomNumber(100);
-
 console.log(randomNumber);
+
+//  Recoger￼ valores
+
+const button = document.querySelector('.js-button');
+console.log(button);
+
+// FUNCIONES
+
+function getInputNumber() {
+  const inputContent = document.querySelector('.js-number');
+  console.log(inputContent.value);
+}
+
+function compareNumber() {}
+
+button.addEventListener('click', getInputNumber);
